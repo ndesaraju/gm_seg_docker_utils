487330f707490e898db46433169cc16ae7d57425
Niharika Desaraju <niharika.desaraju@berkeley.edu>
2020-06-05 22:42:38 -0700

    removing print statement

--- General_reg_par.py
+++ General_reg_par.py
@@ -202,7 +202,7 @@ class SimpleRegister:
 				metric_str='MI['+','.join([self.static_path,os.path.join(self.pth,fil),'1','32'])+']'
 				metric_str1='CC['+','.join([self.static_path,os.path.join(self.pth,fil),'1','3'])+']'
 				metric_str2='Mattes['+','.join([self.static_path,os.path.join(self.pth,fil),'1','32'])+']'
-				print(self.output_path,os.path.join(self.pth, fil),os.path.join(self.pth2,fil2))
+				# print(self.output_path,os.path.join(self.pth, fil),os.path.join(self.pth2,fil2))
 
 				cmd=['antsRegistration', '--dimensionality',str(dim),'--output',os.path.join(self.output_path,'warp'+fil.split('.')[0]),'--transform','Rigid['+str(grad_step)+']','--metric',metric_str2,'--convergence','['+convergence+','+convrg_thresh+',10]','--shrink-factors',str(shrink_factors),'--smoothing-sigmas',str(smoothing_sigmasr),'--transform','Affine['+str(grad_step)+']','--metric',metric_str2,'--convergence','['+convergence+','+convrg_thresh+',10]','--shrink-factors',str(shrink_factors),'--smoothing-sigmas',str(smoothing_sigmas),'--transform','SyN[0.1,2,1]','--metric',metric_str,'--convergence','[500x500x500,1e-6,10]','--shrink-factors','4x1x1','--smoothing-sigmas','1x1x1','--transform','SyN[0.1,2,1]','--metric',metric_str1,'--convergence','[500x500x500,1e-6,10]','--shrink-factors','4x1x1','--smoothing-sigmas','1x1x1']
 

938398e9810acdeb813c2e61d0d9d116bb3af295
Niharika Desaraju <niharika.desaraju@berkeley.edu>
2020-06-17 17:38:19 -0700

    debugging writing log files locally

--- General_reg_par.py
+++ General_reg_par.py
@@ -67,7 +67,8 @@ class SimpleRegister:
 		out = check_output(["ps","-aux"])
 		log.info(out)
 		fin=0
-		# import pdb
+		import pdb
+		pdb.set_trace()
 		count = 0
 		for i in ind:
 			out = []
@@ -239,10 +240,10 @@ class SimpleRegister:
 				job1_array.append((cmd1,cmd2))
 				cmd_array.append(cmd)
 				count+=1
-				# if gilroy:
-				# 	if count>10:
-				# 		self.check_finished(job_array,0)
-				# 		count=0
+				if gilroy:
+					if count>10:
+						self.check_finished(job_array,0)
+						count=0
 
 		if self.check_finished(job_array,0):
 			for i in range(len(self.files)):
